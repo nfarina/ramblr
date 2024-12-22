d1a8bba5480860e79382af5a21c8f07b6bfe0bb5
Nick Farina <nfarina@gmail.com>
2024-12-22 09:28:44 -0800

    Initial Commit

---
+++ WhisperDictate/WhisperDictateApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhisperDictateApp.swift
+//  WhisperDictate
+//
+//  Created by Nick Farina on 12/22/24.
+//
+
+import SwiftUI
+
+@main
+struct WhisperDictateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
